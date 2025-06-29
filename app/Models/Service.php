adbc1e9e85e5ce6cffe0cc2bb1ff287c84cc12f8
MoizAli4444 <moizali1280@gmail.com>
2025-06-30 03:08:10 +0500

    complete sluggable code for service

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -3,8 +3,39 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Service extends Model
 {
-    //
+    use Sluggable;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'image',
+        'status',
+        'created_by',
+        'updated_by',
+    ];
+
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updater()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
 }
