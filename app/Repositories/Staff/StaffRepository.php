b8ecb87029fb42686ae551648ed6397a8f819b20
MoizAli4444 <moizali1280@gmail.com>
2025-07-22 17:54:50 +0500

    create code in staff repository

--- app/Repositories/Staff/StaffRepository.php
+++ app/Repositories/Staff/StaffRepository.php
@@ -2,6 +2,10 @@
 
 namespace App\Repositories\Staff;
 
+use App\Models\Staff;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 class StaffRepository
 {
     /**
@@ -10,5 +14,41 @@ class StaffRepository
     public function __construct()
     {
         //
+    }
+
+
+    public function create(array $data)
+    {
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+
+        $user->assignRole('staff'); // Assign role
+
+        $staffData = [
+            'user_id' => $user->id,
+            'phone' => $data['phone'] ?? null,
+            'address' => $data['address'] ?? null,
+            'date_of_birth' => $data['date_of_birth'] ?? null,
+            'joining_date' => $data['joining_date'] ?? null,
+            'leaving_date' => $data['leaving_date'] ?? null,
+            'is_head' => $data['is_head'] ?? false,
+            'cnic' => $data['cnic'] ?? null,
+            'emergency_contact' => $data['emergency_contact'] ?? null,
+            'shift_id' => $data['shift_id'] ?? null,
+            'working_days' => $data['working_days'] ?? null,
+            'salary' => $data['salary'] ?? null,
+            'payment_schedule' => $data['payment_schedule'] ?? 'monthly',
+            'payment_method_id' => $data['payment_method_id'] ?? null,
+            'bank_account_number' => $data['bank_account_number'] ?? null,
+        ];
+
+        if (isset($data['image'])) {
+            $staffData['image'] = $data['image']->store('staff-images', 'public');
+        }
+
+        return Staff::create($staffData);
     }
 }
