7c7aa742ca48b006cd476ba893e72a67d5c12002
MoizAli4444 <moizali1280@gmail.com>
2025-07-22 17:52:20 +0500

    create function in customer repository

--- app/Repositories/Customer/CustomerRepository.php
+++ app/Repositories/Customer/CustomerRepository.php
@@ -2,13 +2,42 @@
 
 namespace App\Repositories\Customer;
 
+use App\Models\Customer;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 class CustomerRepository
 {
     /**
      * Create a new class instance.
      */
-    public function __construct()
+    public function __construct() {}
+
+    public function create(array $data)
     {
-        //
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+
+        $user->assignRole('customer');
+
+        $customerData = [
+            'user_id' => $user->id,
+            'phone' => $data['phone'] ?? null,
+            'address' => $data['address'] ?? null,
+            'city' => $data['city'] ?? null,
+            'country' => $data['country'] ?? 'pakistan',
+            'postal_code' => $data['postal_code'] ?? null,
+            'date_of_birth' => $data['date_of_birth'] ?? null,
+            'gender' => $data['gender'] ?? null,
+        ];
+
+        if (isset($data['image'])) {
+            $customerData['image'] = $data['image']->store('customer-images', 'public');
+        }
+
+        return Customer::create($customerData);
     }
 }
