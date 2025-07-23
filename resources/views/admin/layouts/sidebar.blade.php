e5c95c53ccccd4a7a4f86fc0b484052c921f07b9
MoizAli4444 <moizali1280@gmail.com>
2025-07-23 12:42:30 +0500

    add staff  route in sidebar file

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -68,13 +68,22 @@
             </a>
         </li>
 
-         <li class="menu-item {{ request()->routeIs('service-variants.*') ? 'active' : '' }}">
+         <li class="menu-item {{ request()->routeIs('staff.*') ? 'active' : '' }}">
+            <a href="{{ route('staff.index') }}" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-spa"></i>
+                <div class="text-truncate" data-i18n="Services">Staff</div>
+            </a>
+        </li>
+
+                 <li class="menu-item {{ request()->routeIs('service-variants.*') ? 'active' : '' }}">
             <a href="{{ route('service-variants.index') }}" class="menu-link">
                 <i class="menu-icon tf-icons bx bx-spa"></i>
                 <div class="text-truncate" data-i18n="Services">Service Variants</div>
             </a>
         </li>
 
+
+        
 
 
         <!-- Dashboards -->
