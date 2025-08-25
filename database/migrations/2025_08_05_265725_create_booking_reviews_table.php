713dae742bb1ea09733d2e003374584ac9f251c8
MoizAli4444 <moizali1280@gmail.com>
2025-08-25 23:36:53 +0500

    update booking reviews migration

--- database/migrations/2025_08_05_265725_create_booking_reviews_table.php
+++ database/migrations/2025_08_05_265725_create_booking_reviews_table.php
@@ -14,10 +14,15 @@ return new class extends Migration
         Schema::create('booking_reviews', function (Blueprint $table) {
             $table->id();
             $table->foreignId('booking_id')->constrained()->onDelete('cascade');
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('customer_id')->constrained()->onDelete('cascade');
             $table->tinyInteger('rating');
             $table->text('review');
             $table->enum('status', ['pending', 'approved', 'rejected'])->default('pending');
+
+            // Polymorphic moderator relation
+            $table->nullableMorphs('moderator');
+            // creates: moderator_id (bigint) + moderator_type (string)
+
             $table->softDeletes();
             $table->timestamps();
         });
