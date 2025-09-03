cfdebea03d013850542d414d3d72a01220a6391c
MoizAli4444 <moizali1280@gmail.com>
2025-09-04 02:02:55 +0500

    update deal migration

--- database/migrations/2025_09_03_140254_create_deals_table.php
+++ database/migrations/2025_09_03_140254_create_deals_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('slug')->unique();
             $table->text('description')->nullable();
+            $table->string('image')->nullable();
 
             $table->decimal('price', 10, 2);
             $table->decimal('services_total', 10, 2)->nullable()
