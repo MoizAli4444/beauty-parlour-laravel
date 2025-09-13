ed1e90293410ca9b9652440fb922af98b7f344c6
MoizAli4444 <moizali1280@gmail.com>
2025-09-13 05:29:21 +0500

    add seeder data

--- database/seeders/SiteSettingsSeeder.php
+++ database/seeders/SiteSettingsSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\SiteSetting;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,21 @@ class SiteSettingsSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        SiteSetting::create([
+            'site_name'       => 'Glamour Beauty Salon',
+            'site_logo'       => 'uploads/settings/logo.png',
+            'favicon'         => 'uploads/settings/favicon.ico',
+
+            'contact_email'   => 'info@glamourbeauty.pk',
+            'contact_phone'   => '+92 321 1234567',
+            'contact_address' => 'DHA Phase 6, Karachi, Pakistan',
+            'working_hours'   => 'Mon-Sun: 10:00 AM – 9:00 PM',
+
+            'facebook_link'   => 'https://facebook.com/glamourbeauty.pk',
+            'instagram_link'  => 'https://instagram.com/glamourbeauty.pk',
+
+            'currency'        => 'PKR',
+            'default_image'   => 'uploads/settings/default.png',
+        ]);
     }
 }
