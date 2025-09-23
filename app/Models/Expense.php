7a0dc7f75f6dd9c6452cabf8409ca9d0ae91c552
MoizAli4444 <moizali1280@gmail.com>
2025-09-24 01:12:03 +0500

    update expense model

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -2,9 +2,33 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Expense extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected $fillable = [
+        'expense_type',
+        'amount',
+        'payment_method',
+        'date',
+        'receipt_path',
+        'notes',
+        'moderator_id',
+        'moderator_type',
+    ];
+
+    /**
+     * Polymorphic relation - who/what the expense was paid to (Admin, Staff, Employee, Supplier, etc.).
+     */
+    public function moderator()
+    {
+        return $this->morphTo();
+    }
 }
